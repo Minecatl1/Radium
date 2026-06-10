a1ae3d66efdf8a9ccbf2f2c2655a80249443b9f6
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2026-06-10 15:15:02 -0500

    Refactor Patch.sh for better maintainability
    Updated script to use variable for plugin directory and improved unzip command.

--- Patch.sh
+++ Patch.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 PATCH_URL="https://cdn.recroomarchive.org/radium/game-client/production/toukeh24kq6w2v4lndyc4z0pblvfyj75/windows/patch.zip"
-SEVENZIP_PATH="$1"
-
-if [ -z "$SEVENZIP_PATH" ]; then
-    echo "Error: 7z binary path not provided."
-    exit 1
-fi
-
+BEP_PLUGIN="BepInEx/plugins"
 curl -L -o patch.zip "$PATCH_URL"
 
-unzip patch.zip
+unzip -v -o patch.zip
 
-mkdir -p "client/BepInEx/plugins"
-mv -f "Radeon.Core.BasePatch.dll" "client/BepInEx/plugins/Radeon.Core.BasePatch.dll"
+mkdir -p "$BEP_PLUGIN"
+mv -f "Radeon.Core.BasePatch.dll" "$BEP_PLUGIN/Radeon.Core.BasePatch.dll"
 
 if [ -f patch.zip ]; then rm -f patch.zip; fi
 
-exit 0
+echo "Done!"
